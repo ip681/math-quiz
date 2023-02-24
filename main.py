e43a8cb6ffb85f6ecff355c06e194a21727eb610
Ivo Petkov <48838737+ip681@users.noreply.github.com>
2023-02-24 19:49:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,63 @@
 import random
 
-QUESTIONS_COUNT = 3
+QUESTIONS_COUNT = 100
 correct_answers_count = 0
 all_answers_count = 0
 
 while all_answers_count < QUESTIONS_COUNT:
 
     num_1 = random.choice(range(1, 99))
-    num_2 = random.choice(range(0, 99))
+    num_2 = random.choice(range(1, 99))
+    operation = random.choice(["+", "-", "*", "/"])
 
-    result = num_1 + num_2
+    if operation == "+":
 
-    print(f"{num_1} + {num_2} = ")
+        result = num_1 + num_2
+        answer = int(input(f"{num_1} + {num_2} = "))
 
-    answer = int(input())
+        if answer == result:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"Wrong! Correct answer is {result}")
 
-    if answer == result:
-        print("Correct!")
-        correct_answers_count += 1
-    else:
-        print(f"Wrong! Correct answer is {result}")
+    elif operation == "-":
+
+        result = num_1 - num_2
+        answer = int(input(f"{num_1} - {num_2} = "))
+
+        if answer == result:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"Wrong! Correct answer is {result}")
+
+    elif operation == "*":
+        num_1 = int(num_1 / 10 + 1)
+        num_2 = int(num_2 / 10 + 1)
+        result = num_1 * num_2
+        answer = int(input(f"{num_1} * {num_2} = "))
+
+        if answer == result:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"Wrong! Correct answer is {result}")
+
+    elif operation == "/":
+        num_2 = int(num_2 / 10 + 1)
+        num_1 = num_2 * random.choice(range(2, 10))
+        result = int(num_1 / num_2)
+        answer = int(input(f"{num_1} / {num_2} = "))
+
+        if answer == result:
+            print("Correct!")
+            correct_answers_count += 1
+        else:
+            print(f"Wrong! Correct answer is {result}")
 
     all_answers_count += 1
+    print(f"Question {all_answers_count} of {QUESTIONS_COUNT}")
 
 print(f"You have {correct_answers_count} correct answers")
 print(f"You have {int(correct_answers_count / all_answers_count * 100)} %")
